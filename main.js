e98d737ddbddd3f2c810f9a2dabdb5bdccc2282e
iw.utan <iw.utan@gmail.com>
2020-11-30 22:29:51 +0900

    load content on page loading

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const editbox = document.getElementById('editbox');
 
 let content = '';
 
-const showMD = () => {
-    content = editbox.innerText;
+const showMD = (new_content) => {
+    content = new_content;
     editbox.innerHTML = marked(content);
     editbox.contentEditable = false;
 };
@@ -14,13 +14,17 @@ const showEdit = () => {
     editbox.contentEditable = true;
 };
 
+const updateURL = () => {
+    location.hash = encodeURI(editbox.innerText);
+};
+
 modeChangeButton.onclick = () => {
     const currentMode = modeChangeButton.getAttribute('data-mode');
     switch(currentMode) {
         case 'edit':
             modeChangeButton.innerText = 'edit';
             modeChangeButton.setAttribute('data-mode', 'view');
-            showMD();
+            showMD(editbox.innerText);
             break;
         case 'view':
             modeChangeButton.innerText = 'view';
@@ -29,5 +33,18 @@ modeChangeButton.onclick = () => {
             break;
         default:
             console.log(`Sorry, we are out of ${currentMode}.`);         
+    }
+};
+
+editbox.oninput = () => {
+    updateURL();
+};
+
+window.onload = () => {
+    const input = decodeURI(location.hash.substr(1));
+    if (input !== '') {
+        showMD(input);
+        modeChangeButton.innerText = 'edit';
+        modeChangeButton.setAttribute('data-mode', 'view');
     }
 };
