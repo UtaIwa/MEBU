bb8a5c90dbeaebd858ee75f41c9210feb06e640a
iw.utan <iw.utan@gmail.com>
2020-11-30 18:24:04 +0900

    Parse Markdown in view mode

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 const modeChangeButton = document.getElementById('changemode');
+const editbox = document.getElementById('editbox');
+
+let content = '';
+
+const showMD = () => {
+    content = editbox.innerText;
+    editbox.innerHTML = marked(content);
+    editbox.contentEditable = false;
+};
+
+const showEdit = () => {
+    editbox.innerText = content;
+    editbox.contentEditable = true;
+};
 
 modeChangeButton.onclick = () => {
     const currentMode = modeChangeButton.getAttribute('data-mode');
@@ -6,10 +20,12 @@ modeChangeButton.onclick = () => {
         case 'edit':
             modeChangeButton.innerText = 'edit';
             modeChangeButton.setAttribute('data-mode', 'view');
+            showMD();
             break;
         case 'view':
             modeChangeButton.innerText = 'view';
             modeChangeButton.setAttribute('data-mode', 'edit');
+            showEdit();
             break;
         default:
             console.log(`Sorry, we are out of ${currentMode}.`);         
